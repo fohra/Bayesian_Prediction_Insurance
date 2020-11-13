0309821102b71b508caa9098ddc7a83f5b85f11c
O44T <63693723+O44T@users.noreply.github.com>
2020-11-13 13:58:23 +0200

    Changed some lines in preprocess
    Changes ensure smooth working of pipeline

--- data/preprocess.R
+++ data/preprocess.R
@@ -2,13 +2,12 @@ preprocess <- function(data){
   #data: dataframe (age, sex, bmi, children, smoker, region, charges)
   #drop children, region
   data <- subset(data, select = -c(children, region))
-  
   #change sex and smoker from factors to numerical
-  data$sex <- as.numeric(data$sex) #female 1, male 2
-  
-  data$smoker <- as.numeric(data$smoker) #no smoking 1, smoking 2
-  data$smoker[data$smoker == 1] <- 0 #no smoking 0, smoking 2
-  data$smoker[data$smoker == 2] <- 1 #no smoking 0, smoking 1
-  
+  data$sex[data$sex == 'female'] <- as.numeric(0) 
+  data$sex[data$sex == 'male'] <- as.numeric(1)
+  data$sex <- as.numeric(data$sex)
+  data$smoker[data$smoker == 'no'] <- as.numeric(0) 
+  data$smoker[data$smoker == 'yes'] <- as.numeric(1)
+  data$smoker <- as.numeric(data$smoker)
   return(data)
 }
